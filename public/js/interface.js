6c9f46e8776dc3382242e101869f3caa76b43349
Myles Borins <myles.borins@gmail.com>
2012-12-12 03:05:45 -0800

    Updated

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,12 +1,30 @@
-/*global jQuery, io, console, socket */
+/*global jQuery, io, window  */
 
-var oscControl = oscControl || {};
+var ifjs = ifjs || {};
 
-(function ($) {
+(function () {
     'use strict';
-    oscControl.socket = io.connect('http:/basestation.local:1337');
+    ifjs.model = {
+        tilt: {
+            alpha: 0,
+            beta: 0,
+            gamma: 0
+        }
+    };
+    
+    ifjs.socket = io.connect('http:/basestation.local:1337');
+    
+    ifjs.sendTilt = function() {
+        ifjs.socket.emit('alpha', ifjs.model.tilt.alpha);
+        ifjs.socket.emit('beta', ifjs.model.tilt.beta);
+        ifjs.socket.emit('gamma', ifjs.model.tilt.gamma);
+    };
+    
 
-
-    });
-    oscControl.pad.css({'border': '5px solid #BBB'});
+    ifjs.accel = window.addEventListener("deviceorientation", function(event) {
+        // Update model with events
+        ifjs.model.tilt.alpha = event.alpha;
+        ifjs.model.tilt.beta = event.beta;
+        ifjs.model.tilt.gamma = event.gamma;
+    }, true);
 }(jQuery));
